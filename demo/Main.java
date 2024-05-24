3e934ec5be9ef95f35f2200df52bac88ed8a3798
ShrenaShah <121631745+ShrenaShah@users.noreply.github.com>
2024-05-24 18:43:11 +0530

    Delete demo directory

--- demo/Main.java
+++
@@ -1,9 +0,0 @@
-class Main 
-{
-    public static void main(String args[])
-    {
-        DefiningGenerics<Integer> a = new DefiningGenerics<>();
-        a.set(10);
-        System.out.println("value: " + a.get());
-    }
-}
