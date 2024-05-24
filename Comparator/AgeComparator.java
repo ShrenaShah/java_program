b5278915050686957dd6841882dc82a986827474
ShrenaShah <121631745+ShrenaShah@users.noreply.github.com>
2024-05-24 18:42:08 +0530

    Delete Comparator directory

--- Comparator/AgeComparator.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-public class AgeComparator implements Comparator<Person>
-{
-    public int compare(Person p1, Person p2)
-    {
-        return Integer.compare(p1.getAge(), p2.getAge());
-    }
-}
