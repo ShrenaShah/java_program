7a93e7b67b1af0d12f7cd5697d84b119f9fb5208
ShrenaShah <121631745+ShrenaShah@users.noreply.github.com>
2024-05-24 18:41:06 +0530

    Delete Bounded type generics directory

--- Bounded type generics/Main.java
+++
@@ -1,8 +0,0 @@
-class Main  
-{
-    public static void main(String args[])
-    {
-        Addition<Number> t = new Addition<>();
-        System.out.println("Addition of two number 5 and 10: "+ t.sum(5,10));
-    }
-}
